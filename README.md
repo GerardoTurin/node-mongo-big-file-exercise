1b2e16583691e0730911ff36d9a47f08c95112d0
Gerardo Turin <gera_1993_06@hotmail.com>
2025-06-21 16:29:13 -0300

    Final Test Complete

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Hola! Este es un ejercicio para poner a prueba tus conocimientos de NodeJS y MongoDB. El objetivo es realizar un endpoint que reciba un archivo de ~80mb separado por comas y guarde cada uno de los registros del archivo en la base de datos.
 
 El archivo podés descargarlo de este link:
-https://drive.google.com/file/d/1tg8dWr4RD2CeKjEdlZdTT8kLDzfITv_S/view?usp=sharing
+<https://drive.google.com/file/d/1tg8dWr4RD2CeKjEdlZdTT8kLDzfITv_S/view?usp=sharing>
 (está zippeado para que lo descargues rápido, descomprimilo manualmente)
 
 Se evaluará teniendo en cuenta la prolijidad del código (indentación, comentarios y legibilidad), la performance (tiempo de procesado y memoria utilizada) y escalabilidad (si soporta archivos aún más grandes).
@@ -19,5 +19,36 @@ Para simplificarlo, hemos creado este repo starter que se conecta a la base de d
 - El archivo subido se guarda en el directorio `_temp`, recordá eliminarlo luego de utilizarlo.
 - Modificá el archivo `.env` para cambiar el puerto y la conexión a la base de datos.
 
-## Postman
-En el directorio `postman` del repo, vas a encontrar los dos requests para que puedas importarlos en Postman.
+
+
+
+
+
+
+
+
+
+## Notas sobre esta prueba tecnica
+
+### Pasos realizados para la carga del archivo CSV
+
+1. **Recepción del archivo:**  
+   El endpoint `/upload` recibe el archivo CSV y lo almacena temporalmente en al carpeta _temp.
+
+2. **Lectura y procesamiento por lotes:**  
+   Se lee el archivo utilizando streams para evitar cargarlo completamente en memoria. Los registros se agrupan en lotes de 1000 antes de ser insertados en la base de datos con operaciones `bulkWrite`, optimizando así el rendimiento y reduciendo la carga sobre MongoDB.
+
+3. **Eliminación del archivo temporal:**  
+   Una vez procesado el archivo, se elimina del directorio `_temp` para liberar espacio en disco.
+
+### Cambio de `csv-parser` a `fast-csv`
+
+Inicialmente utilice la librería `csv-parser`, pero se presentaron inconvenientes:
+
+- **Manejo de streams y backpressure:**  
+  `csv-parser` no ofrece un control tan fino sobre el flujo de datos y el manejo de la presión de backpressure, lo que dificultaba pausar y reanudar el procesamiento cuando los lotes/batchs estaban siendo insertados.
+
+- **Compatibilidad y flexibilidad:**  
+  `fast-csv` permitie una integración más sencilla con los streams y facilitó el procesamiento eficiente de archivos grandes.
+
+Por esto, cambie a `fast-csv` para el procesamiento del archivo CSV en este proyecto.
--- src/controller.js
+++ src/controller.js
@@ -1,12 +1,94 @@
+const fs = require('fs');
+const fastCsv = require('fast-csv');
 const Records = require('./records.model');
+const { pipeline, Transform } = require('stream');
+const { promisify } = require('util');
+const pipelineAsync = promisify(pipeline);
 
 const upload = async (req, res) => {
-    const {file} = req;
 
-    /* Acá va tu código! Recordá que podés acceder al archivo desde la constante file */
+    // TODO: Medir rendimiento ( variables ), descomentar para verlo en consola.
+    //const startTime = process.hrtime();
+    //const startHeap = process.memoryUsage().heapUsed;
 
-    return res.status(200).json({ message: 'some response' });
+
+    if (!req.file) return res.status(400).json({ error: 'Debes agregar un Archivo' });
+
+    const BATCH_SIZE = 1000;    // Tamaño del batch para bulkWrite. 1000 registros.
+    const batch = [];
+
+    const bulker = new Transform({
+        objectMode: true,   // Permitir trabajar con objetos.
+        transform(row, _, done) {   // Transformar cada fila del CSV en un objeto para insertarlo.
+            batch.push({
+                insertOne: {
+                    document: {
+                        id: +row.id,
+                        firstname: row.firstname,
+                        lastname: row.lastname,
+                        email: row.email,
+                        email2: row.email2,
+                        profession: row.profession
+                    }
+                }
+            });
+
+            if (batch.length >= BATCH_SIZE) {
+                this.pause(); // Si el batch está lleno, pausar el stream
+                
+                // Ejecutar bulkWrite para insertar el batch
+                Records.collection
+                    .bulkWrite(batch, { ordered: false, w: 0 }) // w: 0: Es para no esperar confirmación
+                    .then(() => { batch.length = 0; this.resume(); done() })    // Limpiar el batch y reanudar el stream
+                    .catch(done);
+
+            } else done();  // Si no está lleno, continuar con el siguiente registro
+        },
+        final(done) {
+            if (!batch.length) return done();       // Si el batch está vacío, termina.
+
+            // Si hay registros pendientes, ejecutar bulkWrite
+            Records.collection  
+                .bulkWrite(batch, { ordered: false, w: 0 })
+                .then(() => { batch.length = 0; done() })
+                .catch(done);
+        }
+    });
+
+    try {
+        // Leo el archivo CSV y proceso.
+        await pipelineAsync(
+            fs.createReadStream(req.file.path, { highWaterMark: 16 * 1024 }),   // Leo en bloques de 16KB
+            fastCsv.parse({ headers: true }),   // Parsear el CSV con encabezados para que los nombres de las columnas sean accesibles
+            bulker
+        );
+
+        res.status(200).json({ message: 'Archivo procesado y guardado Correctamente' });
+    } catch {
+        res.status(500).json({ error: 'Error procesando archivo.' });
+
+    } finally {
+        fs.unlink(req.file.path, () => { });    // Eliminar el archivo de _temp después de procesarlo
+    }
+
+
+    // TODO: Medir rendimiento, descomentar para verlo en consola.
+
+    /* res.on('finishProcess', () => {
+        const [sec, nan] = process.hrtime(startTime);          // CalculO tiempo
+        const respMs = sec * 1000 + nan / 1e6;               // Convierto a milisegundos
+        console.log(`[SubidaArchivo] Tiempo de respuesta: ${respMs.toFixed(2)} ms`);
+
+
+        const endHeap = process.memoryUsage().heapUsed;
+        const diffMB = (endHeap - startHeap) / 1024 / 1024;
+        console.log(`[SubidaArchivo] Consumo neto de heap: ${diffMB.toFixed(2)} MB`);
+    }); */
 };
+
+
+
+
 
 const list = async (_, res) => {
     try {
@@ -14,7 +96,7 @@ const list = async (_, res) => {
             .find({})
             .limit(10)
             .lean();
-        
+
         return res.status(200).json(data);
     } catch (err) {
         return res.status(500).json(err);
